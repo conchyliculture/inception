8db68a6f89d46a0ba1661c8d9403b85f4282f7a5
renzo <renzo@exemple.com>
2014-08-25 20:00:11 +0200

    +xtw.me +wk.tk +skroc.pl +xurl.pl +sy.pe +got.pl +u42pl +xls.cz

--- inception.rb
+++ inception.rb
@@ -9,6 +9,9 @@ class Shortener
     def name ()
         return self.class
     end
+    def picky()
+        return false
+    end
 end
 
 ######### POST
@@ -23,6 +26,10 @@ class GooGL < Shortener
         else
             return res["id"]
         end
+    end
+
+    def picky
+        return true
     end
 
 end
@@ -112,8 +119,102 @@ class NqSt < Shortener
         end
         return lolilol 
     end
-
 end
+
+class XtwMe < Shortener
+    def get_short(url)
+        http=Net::HTTP.new("xtw.me")
+        res =http.post("/", URI.encode_www_form({"url"=>url,"btn_submit.x"=>"75","btn_submit.y"=>"20"}))
+        lolilol=nil
+        res.body.each_line do |l|
+            if l=~/<input type='text' size='50' value='([^']+)' \/><\/div>/
+                lolilol = $1
+            end
+        end
+        return lolilol 
+    end
+end
+
+class WkTk < Shortener
+    def get_short(url)
+        http=Net::HTTP.new("wk.tk")
+        res =http.post("/create", URI.encode_www_form({"url"=>url}))
+        lolilol=JSON.parse(res.body)
+        if lolilol["error"]
+            return nil
+        else
+            return lolilol["sURL"]
+        end
+    end
+
+    def picky
+        return true
+    end
+end
+class SkrocPl < Shortener
+    def get_short(url)
+        http=Net::HTTP.new("home.skroc.pl")
+        res =http.post("/create.php", URI.encode_www_form({"url"=>url,"adres"=>"skroc","skroc"=>"/","expired"=>"","password"=>""}))
+        lolilol=nil
+        res.body.each_line do |l|
+            if l=~/<input readonly="readonly" style="width: 250px; text-align:center; font-size: 16px;" name="k1" value="([^"]+)"/
+                lolilol=$1
+            end
+        end
+        return lolilol
+    end
+
+    def picky
+        return true
+    end
+end
+class XurlPl < Shortener
+    def get_short(url)
+        http=Net::HTTP.new("xurl.pl")
+        res =http.post("/index.html", URI.encode_www_form({"longUrl"=>url,"shortUrlDomain"=>"1","submitted"=>"1","customUrl"=>"","shortUrlPassword"=>"","shortUrlExpiryDate"=>"","shortUrlUses"=>"0","shortUrlType"=>"0"}))
+        res=http.get(res["location"]) # lolilol!
+        res=http.get(res["location"]) # lolilol!   
+        lolilol=nil
+        res.body.each_line do |l|
+            if l=~/<a href="(http:\/\/xurl.pl\/[^"]+)" target="_blank"/
+                lolilol=$1
+            end
+        end
+        return lolilol
+    end
+
+    def picky
+        return true
+    end
+end
+
+
+##FAIIIIIL
+#class IcbMe < Shortener
+#    def get_short(url)
+#        http=Net::HTTP.new("icb.me")
+#        lol=http.get("/")
+#        pute1=""
+#        pute2= lol["set-cookie"][/^([^;]+);/,1]
+#        puts pute2
+#        lol.body.each_line do |l|
+#            case l 
+#            when /<input type="hidden" name="form_build_id" id="(form-[^"]+)"/
+#                pute1=$1
+#            end
+#        end
+#        res =http.post("/", URI.encode_www_form({"long_url"=>url,"op"=>"Shrink it","form_build_id"=>pute1,"form_id"=>"shurly_create_form"}),{"Cookie"=>pute2})
+#        lolilol=nil
+#        res.body.each_line do |l|
+#            puts l
+#            if l=~/<a href="(http:\/\/nq.st\/[^"]+)">http:\/\/nq.st\/([^"]+)<\/a>/
+#                lolilol = $1
+#            end
+#        end
+#        return lolilol 
+#    end
+#
+#end
 
 ######### GET
 class SCoop < Shortener
@@ -194,6 +295,40 @@ class UrlzFr < Shortener
     end
 end
 
+class SyPe < Shortener
+    def get_short(url)
+        res=Net::HTTP.get(URI.parse("http://sy.pe/save_n_check_url.php?url=#{url}"))
+        return res[/value='([^']+)'/,1]
+    end
+end
+
+class GotPl < Shortener
+    def get_short(url)
+        res=Net::HTTP.get(URI.parse("http://got.pl/api?x=#{url}&domain=on"))
+        return JSON.parse(res[3..-1])["content"]
+    end
+end
+
+class U42Pl < Shortener
+    def get_short(url)
+        res=Net::HTTP.get(URI.parse("http://u.42.pl/index.php?descr=&url=#{url}"))
+        puts res
+        return res[/<a href="(http:\/\/u.42.pl\/[^"]+)"/,1]
+    end
+end
+
+class XlsCz < Shortener
+    def get_short(url)
+        res=Net::HTTP.get(URI.parse("http://xsl.cz/index.php?alias=&url=#{url}"))
+        res.each_line do |l|
+            if l=~/<p>Short URL: <strong><a href="([^"]+)"/
+                return $1
+            end
+        end
+        return nil
+    end
+end
+
 class Inception
     def initialize()
         @slist=[]
@@ -217,11 +352,14 @@ end
 
 i=Inception.new()
 
-#i.add_shortener(PpirAt.new())
-#i.inception('http://www.free.fr')
+#i.add_shortener(GotPl.new())
+#i.inception('http://www.free.fr/lol')
 #exit
 
 i.add_shortener(
+                XlsCz.new(),
+                U42Pl.new(),
+                XurlPl.new(),
                 AresTl.new(),
                 IsGd.new(),
                 CpcCx.new(),
@@ -231,9 +369,12 @@ i.add_shortener(
                 UrlzFr.new(),
                 JoiNu.new(),
                 PpirAt.new(),
+                XtwMe.new(),
                 AresTl.new,
                 VGd.new(),
                 PpfrIt.new(),
+                SkrocPl.new(),
+                GotPl.new(),
                 TinyURL.new(),
                 VaMu.new(),
 #                DurlMe.new(),
